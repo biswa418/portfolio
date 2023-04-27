632b4a869311aa26ba543470c18cbdab97011d98
Biswajeet Sutar <biswajeetsutar418@gmail.com>
2023-04-27 20:12:15 +0530

    nav complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,13 +5,24 @@ import Navbar from './Navbar'
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+`
 
 const Hero = () => {
     return (
         <Section>
-            <Navbar/>
+            <Navbar />
+            <Container className='w-3/4 flex justify-between max-[768px]:w-full max-[768px]:flex-col max-[768px]:items-center max-[768px]:justify-center'>
+                Hero
+            </Container>
         </Section>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { FaSearch } from 'react-icons/fa';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <nav className='flex justify-center md:w-full'>
+            <div className='flex justify-between py-3 items-center w-3/4 max-[768px]:w-full max-[768px]:p-3'>
+                <div className='flex items-center'>
+                    <img src='./images/white-logo.png' className='h-9' alt='logo' />
+                    <p className='text-xl px-2 block w-full max-[768px]:hidden'> Biswajeet Sutar </p>
+                </div>
+
+                <ul className='flex max-[768px]:hidden'>
+                    <li className='px-2 cursor-pointer'>Home</li>
+                    <li className='px-2 cursor-pointer'>Experience</li>
+                    <li className='px-2 cursor-pointer'>Projects</li>
+                    <li className='px-2 cursor-pointer'>Contact</li>
+                </ul>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
