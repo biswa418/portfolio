777e4bd6d92102d5d0e03a6a1c29cac4acad1765
Biswajeet Sutar <biswajeetsutar418@gmail.com>
2023-04-27 18:02:31 +0530

    Set up home page and bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './components'
+import { Contact, Hero, Who, Work } from './components'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: rebeccapurple;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: linear-gradient(-50deg, rgba(2,0,36,1) 0%, rgba(74,9,89,1) 49%, rgba(22,12,135,1) 100%);
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container>
+      <Hero />
+      <Work />
+      <Who />
+      <Contact />
+    </Container>
   )
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import Contact from './Contact';
+import Hero from './Hero';
+import Who from './Who';
+import Work from './Work';
+
+
+export {
+    Contact,
+    Hero,
+    Who,
+    Work 
+}
