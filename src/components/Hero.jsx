ee63c30feb0118dcd6fdf61b8d4e11b020c89913
biswa418 <biswajeetsutar418@gmail.com>
2023-04-28 02:08:25 +0530

    almost responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,6 +11,7 @@ const Section = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    overflow: hidden;
 `
 
 const Container = styled.div`
@@ -60,19 +61,20 @@ const Hero = () => {
         <Section id='#'>
             <Navbar />
             <Container className='w-4/5 flex justify-between relative items-center max-[768px]:px-5 max-[768px]:w-full max-[768px]:flex-col max-[768px]:items-center max-[768px]:justify-center'>
-                <Left className='flex-[2_0_0%] pl-4 flex flex-col justify-center'>
+                <Left className='flex-[2_2_0%] pl-4 flex flex-col justify-center'>
 
-                    <div className='text-5xl max-[768px]:text-2xl max-[768px]:mt-4'>I'm a &nbsp;
+                    <span className='text-5xl max-[768px]:text-2xl max-[768px]:mt-4'>I'm a </span>
+                    <div className='text-5xl max-[768px]:text-2xl'>
                         <span className='text-6xl max-[768px]:text-3xl text-[#e57ebd]' ref={el} />
                     </div>
-                    <div className='text-2xl max-[768px]:text-lg'>
+                    <div className='text-2xl max-[768px]:text-lg z-[1] mt-4 max-[768px]:m-0'>
                         With 2 years of experience in the industry.
                         Enthusiastic team player, a fast learner with good problem-solving skills,
                         my professional interests include Web Development,
                         Software Application development, and Research.
                     </div>
 
-                    <button className='absolute cursor-pointer z-10 bottom-9 right-2/4 text-5xl mt-4 animate-bounce text-right text-[#A6176C]' onClick={(e) => {
+                    <button className='absolute cursor-pointer z-10 left-0 right-0 bottom-8 m-auto text-5xl mt-4 animate-bounce text-[#A6176C]' onClick={(e) => {
                         e.preventDefault();
                         window.location.href = '#experience';
                     }}>
@@ -80,7 +82,7 @@ const Hero = () => {
                     </button>
                 </Left>
 
-                <Right className='flex-[3_0_0%] relative flex justify-center'>
+                <Right className='flex-[3_2_0%] relative flex justify-center max-[768px]:w-[600px]'>
                     <Img className='w-[800px] h-[800px] object-contain absolute top-0 bottom-0 left-0 right-0 m-auto' src='./images/bro.png' alt='hero-pic' />
                 </Right>
             </Container>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <nav className='flex justify-center md:w-full z-10'>
             <div className='flex justify-between py-3 items-center w-4/5 max-[768px]:w-full max-[768px]:p-3'>
-                <div className='flex items-center'>
+                <div className='flex items-center cursor-pointer' onClick={() => { e.preventDefault(); window.location.href = '#'; }}>
                     <img src='./images/white-logo.png' className='h-9 max-[768px]:mt-3' alt='logo' />
                     <p className='text-xl px-2 block w-full max-[768px]:hidden'> Biswajeet Sutar </p>
                 </div>
