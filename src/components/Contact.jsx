c6fa31fb0f11d3e19b4e7caeb2a847105174a9bb
Biswajeet Sutar <biswajeetsutar418@gmail.com>
2023-04-28 23:04:20 +0530

    contact page responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BsDownload, BsGithub, BsLinkedin, BsMailbox2 } from 'react-icons/bs'
+import { BsDownload, BsFillArrowUpCircleFill, BsGithub, BsLinkedin, BsMailbox2 } from 'react-icons/bs'
 import styled from 'styled-components'
 
 const Section = styled.div`
@@ -32,25 +32,26 @@ const Right = styled.div`
 const Contact = () => {
   return (
     <Section id='contact'>
-      <Container className='flex justify-between flex-col md:flex-row'>
+      <Container className='flex justify-between flex-col md:flex-row relative'>
         <Left className='items-end justify-center flex-col'>
-          <div className='mx-auto flex flex-col justify-center items-center h-2/3 lg:h1/3 w-2/3 lg:w-1/3 '>
+          <div className='mx-auto flex flex-col justify-center items-center h-1/3 lg:h1/3 w-2/3 lg:w-1/3 '>
             <h2 className='font-semibold text-4xl uppercase mb-5'>CONNECT</h2>
+
             <ul className='flex flex-col justify-between w-full '>
               <div className='flex justify-evenly items-center mb-4'>
-                <li className='text-xl lg:text-3xl lg:py-4 lg:px-4 font-medium z-10 px-2 py-2 flex items-center w-max gap-4 justify-center rounded-full bg-teal-200 ring-2 sm:ring-4 ring-teal-300/30 text-teal-700'>
+                <li className='text-xl md:text-3xl mr-2 md:py-4 md:px-4 font-medium z-10 px-2 py-2 flex items-center w-max gap-4 justify-center rounded-full bg-teal-200 ring-2 sm:ring-4 ring-teal-300/30 text-teal-700'>
                   <a href='https://www.github.com/biswa418' target='_blank'>
                     <BsGithub />
                   </a>
                 </li>
 
-                <li className='text-xl lg:text-3xl lg:py-4 lg:px-4 font-medium z-10 px-2 py-2 flex items-center w-max gap-4 justify-center rounded-full bg-blue-200 ring-2 sm:ring-4 ring-blue-300/30 text-blue-700'>
+                <li className='text-xl md:text-3xl mr-2 md:py-4 md:px-4 font-medium z-10 px-2 py-2 flex items-center w-max gap-4 justify-center rounded-full bg-blue-200 ring-2 sm:ring-4 ring-blue-300/30 text-blue-700'>
                   <a href='https://www.linkedin.com/in/biswajeet-sutar' target='_blank'>
                     <BsLinkedin />
                   </a>
                 </li>
 
-                <li className='text-xl lg:text-3xl lg:py-4 lg:px-4 font-medium z-10 px-2 py-2 flex items-center w-max gap-4 justify-center rounded-full bg-rose-200 ring-2 sm:ring-4 ring-rose-300/30 text-rose-700'>
+                <li className='text-xl md:text-3xl mr-2 md:py-4 md:px-4 font-medium z-10 px-2 py-2 flex items-center w-max gap-4 justify-center rounded-full bg-rose-200 ring-2 sm:ring-4 ring-rose-300/30 text-rose-700'>
                   <a href='mailto:biswajeetsutar418@gmail.com' target='_blank'>
                     <BsMailbox2 />
                   </a>
@@ -58,7 +59,7 @@ const Contact = () => {
               </div>
 
               <li>
-                <a href='./public/Biswajeet_cv.pdf' draggable='false' className='text-xl mx-auto lg:text-3xl lg:py-3 lg:px-5 font-medium z-10 px-2 py-1 flex items-center w-max gap-4 justify-center rounded-full bg-fuchsia-200 ring-2 sm:ring-4 ring-fuchsia-300/30 text-fuchsia-700' target="_blank" rel="noopener noreferrer" download>
+                <a href='./public/Biswajeet_cv.pdf' draggable='false' className='text-xl mx-auto lg:text-3xl lg:py-3 lg:px-5 font-medium z-10 px-2 py-1 md:my-3 flex items-center w-max gap-4 justify-center rounded-full bg-fuchsia-200 ring-2 sm:ring-4 ring-fuchsia-300/30 text-fuchsia-700' target="_blank" rel="noopener noreferrer" download>
                   <button className='cursor-pointer font-medium text-xl lg:text-3xl text-fuchsia-700 mr-[-10px]'>
                     <BsDownload />
                   </button>
@@ -68,7 +69,7 @@ const Contact = () => {
             </ul>
           </div>
 
-          <form className='w-10/12 md:w-11/12 max-w-screen-lg max-[768px]:flex max-[768px]:flex-col max-[768px]:h-max max-[768px]:gap-2 max-[768px]:px-4 max-[768px]:py-6 gap-8 px-8 py-16 my-6 mx-auto md:mx-0 text-purple-900 bg-purple-200 md:px-12 lg:px-16 rounded-lg ring-4 ring-purple-200/30' action="" method="post">
+          <form className='w-10/12 md:w-11/12 min-w-[270px] max-w-screen-lg max-[768px]:flex max-[768px]:flex-col max-[768px]:h-max max-[768px]:gap-2 max-[768px]:px-4 max-[768px]:py-6 gap-8 px-8 py-16 my-6 mx-auto md:mx-0 text-purple-900 bg-purple-200 md:px-12 lg:px-16 rounded-lg ring-4 ring-purple-200/30' action="" method="post">
             <h2 className='font-semibold text-4xl uppercase mb-4 max-[768px]:m-0'>Contact</h2>
 
             <label className='flex flex-col mb-2 max-[768px]:m-0'>
@@ -95,6 +96,13 @@ const Contact = () => {
         <Right>
 
         </Right>
+
+        <button className='absolute cursor-pointer z-10 left-0 right-0 bottom-0 m-auto text-5xl mt-4 animate-bounce text-[#A6176C]' onClick={(e) => {
+          e.preventDefault();
+          window.location.href = '#home';
+        }}>
+          <BsFillArrowUpCircleFill />
+        </button>
       </Container>
     </Section>
   )
