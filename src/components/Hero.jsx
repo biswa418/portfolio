62fa658ad15c3699ea5f8e0ccdf5010ad87ea99a
biswa418 <biswajeetsutar418@gmail.com>
2023-05-09 12:58:59 +0530

    clean up

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,6 @@ import Typed from 'typed.js'
 import { BsFillArrowDownCircleFill, BsDownload } from 'react-icons/bs'
 import { Canvas } from '@react-three/fiber'
 import { MeshDistortMaterial, OrbitControls, Sphere } from '@react-three/drei'
-import { Color } from 'three'
 
 const Section = styled.div`
     height: 100vh;
@@ -91,17 +90,19 @@ const Hero = () => {
 
                 <Right className='flex-[3_0_0%] relative flex justify-center max-[768px]:w-[500px] h-2/3 lg:h-full'>
                     <Canvas camera={{ fov: 25, position: [5, 5, 5] }}>
-                        <OrbitControls enableZoom={false} />
-                        <ambientLight intensity={1} color={'darkblue'} />
-                        <directionalLight position={[1, 2, 1]} color={'red'} intensity={0.2} />
-                        <pointLight position={[-4, -5, -10]} color={'red'} intensity={0.2} />
-                        <Sphere args={[1, 100, 200]} scale={1.5} >
-                            <MeshDistortMaterial
-                                attach="material"
-                                distort={0.5}
-                                speed={2}
-                            />
-                        </Sphere>
+                        <Suspense fallback={null}>
+                            <OrbitControls enableZoom={false} />
+                            <ambientLight intensity={1} color={'darkblue'} />
+                            <directionalLight position={[1, 2, 1]} color={'red'} intensity={0.2} />
+                            <pointLight position={[-4, -5, -10]} color={'red'} intensity={0.2} />
+                            <Sphere args={[1, 100, 200]} scale={1.5} >
+                                <MeshDistortMaterial
+                                    attach="material"
+                                    distort={0.5}
+                                    speed={2}
+                                />
+                            </Sphere>
+                        </Suspense>
                     </Canvas>
                     <Img className='w-[200px]  md:w-[600px] min-[1920px]:w-[1000px] min-w-[400px] object-contain absolute top-0 bottom-0 left-0 right-0 m-auto' src='./images/bro.png' alt='hero-pic' />
                 </Right>
