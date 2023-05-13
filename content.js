6523b7de1c16797d80d5d3b8c9a599ae07abbb41
biswa418 <biswa418@users.noreply.github.com>
2023-05-13 11:54:20 +0000

    deploy: 542b604af99602e14977fd5847daddc3a7945eca

---
+++ content.js
@@ -0,0 +1,45 @@
+const project = {
+    "name": [
+        "Employee Review",
+        "Placement Cell",
+        "retro iPod Clone",
+        "DevConnect",
+        "DevConnect UI"
+    ],
+    "desc": [
+        "Making review system of any company easier. Authentication is needed to post the review. For both Admin-Employee",
+        "Keeps all the records of the college placements. For employee use only. Schedules interview for students",
+        "Feeling old yet? Experience the retro iPod UI made using React. With all gesture support and pre-loaded songs.",
+        "A social media app, connect with your friends. Post your thoughts, update your profile, comment on the post and chat with your friends.",
+        "The UI part of the social media. Connects with the backend using APIs."
+    ],
+    "source": [
+        "./images/projects/placement.gif",
+        "./images/projects/placement1.gif",
+        "./images/projects/inAction.gif",
+        "./images/projects/dev.gif",
+        "./images/projects/currentState.png"
+    ],
+    "link": [
+        "https://github.com/biswa418/employee-review",
+        "https://github.com/biswa418/placement-Cell",
+        "https://github.com/biswa418/ipod-ui-react",
+        "https://github.com/biswa418/socialMedia",
+        "https://github.com/biswa418/socialMedia-react"
+    ],
+    "host": [
+        "https://employee-review-jh1l.onrender.com/",
+        "https://placement-cell-52w7.onrender.com/",
+        "https://codesandbox.io/p/github/biswa418/ipod-ui-react/"
+    ],
+    "video": [
+        "https://youtu.be/FVQGBKbpBhM",
+        "https://youtu.be/Se0MS0WF2BA",
+        "",
+        "https://youtu.be/dDDqNfgib7Y",
+        ""
+    ]
+}
+
+
+export default project;
